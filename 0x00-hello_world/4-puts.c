46d047ac4e6bff2795cf17f529da93ef85a85d73
Gaviro <talentagaviro@gmail.com>
2022-06-30 03:51:45 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
 
-
-
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+  *main - Entry point
+  *
+  *Return: Always 0 (Success)
+  */
 
 int main(void)
 
@@ -18,6 +12,6 @@ int main(void)
 
 		puts("\"Programming is like building a multilingual puzzle");
 
-			return (0);
+		return (0);
 
 }
