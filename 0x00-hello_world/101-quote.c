5153f88a7967ac8fc01a7193423191d2aafd6443
Gaviro <talentagaviro@gmail.com>
2022-06-30 05:34:37 -0700

    advanced task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
   *main - Entry point
   *
   *Return: Always 1 (Success)
   */
-int main(void)
+int main()
 {
-
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
-
+       	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
-
 }
