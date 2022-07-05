4b01a036fbb10cc77618daf68dd44d01b54214ed
Gaviro <talentagaviro@gmail.com>
2022-07-05 00:51:26 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * Main - prints alphabrt in lowerrcase
+ *
+ * Return (o)
+ */
+void print_alphabet(void);
+{
+	int i:
+
+		for(i='a';i<='z';i++)
+		{
+			_putchar(i);
+		}
+	_putchar('\n')
+}
+
