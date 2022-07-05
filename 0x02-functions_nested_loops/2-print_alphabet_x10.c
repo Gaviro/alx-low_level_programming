ff40b26994c0bf07ac133a35c9f29406eef5d0c7
Gaviro <talentagaviro@gmail.com>
2022-07-05 04:23:58 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,14 @@
 #include "main.h"
+
 /**
  * main - prints alphabets 10 times in lower case
+ * Description: function uses _putchar function to print
+ * Return: Nothing
  */
+
 void print_alphabet_x10(void)
 {
-	int i;
+	char i;
 	int j;
 
 	for (j = 0; j <= 10; j++)
