2f9ec05203ad38430040c4d56cd51bcea26d7079
Gaviro <talentagaviro@gmail.com>
2022-07-05 01:08:56 -0700

    hot fix task 2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,7 +4,7 @@
  *
  * Return (o)
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	int i:
 
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - prints alphabets 10 times in lower case
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for(j=0;j<=10;j++)
+	{
+		for(i='a';i<='z';i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
