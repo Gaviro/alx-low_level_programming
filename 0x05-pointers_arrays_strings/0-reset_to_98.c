b5ed75dd7663e7d4d97cd76f9dd9053163c73e6d
Gaviro <talentagaviro@gmail.com>
2022-07-11 06:33:36 -0700

    98 Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code 
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int n;
+	*p = &n;
+	n = 402;
+	
+	printf("n=%d\n", n);
+	*p = 98;
+	printf("n=%d\n", n);
+	
+	return (0);
+}
